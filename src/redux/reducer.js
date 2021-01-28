5bfcb9f7178a8701545005ac6954894ae8b26f6f
PalnaRoman <roma.palna@gmail.com>
2021-01-28 11:14:43 +0200

    test

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,12 +1,12 @@
-import { createReducer } from "@reduxjs/toolkit";
-import { combineReducers } from "redux";
-import actions from "./actions";
+import { createReducer } from '@reduxjs/toolkit';
+import { combineReducers } from 'redux';
+import actions from './actions';
 
 const initialState = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
 ];
 
 const items = createReducer(initialState, {
@@ -16,7 +16,7 @@ const items = createReducer(initialState, {
     state.filter(({ id }) => id !== action.payload),
 });
 
-const filter = createReducer("", {
+const filter = createReducer('', {
   [actions.filter]: (_, action) => action.payload,
 });
 
